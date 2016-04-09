d38beca4f205677d8c4b3bd0f5836f1be9781cb9
JohnnyHorse <jackylikebmw@yahoo.com.tw>
2016-04-09 22:06:36 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
+
+#I didn't do much things.Just follow the sample code and write down the code.
+#The only thing change is some simple variables.
+
+
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
+#Instead of the mean function in the sample code,I use the "solve" function.
+#!is.null(m) can verifies whether it has been calculated before or not.If calculated before print "getting cached data".
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
